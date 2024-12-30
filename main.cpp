4d4b642d7649bdafeb96f0d3f31f6b237c5017fe
Aarush-sharma <aarush.sharma.fpw@gmail.com>
2024-12-31 01:27:14 +0530

    added: help guide

--- main.cpp
+++ main.cpp
@@ -1,10 +1,72 @@
-#include "src/arithmetic.h"
-#include "src/magic.h"
-#include <bits/stdc++.h>
+
+#include "src/tools.hpp"
 using namespace std;
 
-int main(int argc, char *argv[]) {
-   vector<int> nums = {1,2,5};
-    math::sum(nums);
-    return 0;
+int main(int argc, char *argv[])
+{
+   string query = argv[1];
+   if (query == "-q")
+   {
+      string task = argv[2];
+      string res = cook(task);
+      cout << res << endl;
+   }
+   else if (query == "greet")
+   {
+      cout << magic::greet() << endl;
+   }
+   else if (query == "roll")
+   {
+      int s = stoi(argv[2]);
+      int k = magic::roll(s);
+      cout << k << endl;
+   }
+   else if (query == "sort")
+   {
+      vector<int> nums;
+      for (int i = 2; i < argc; i++)
+      {
+         nums.push_back(stoi(argv[i]));
+      }
+      magic::sort(nums, 0, nums.size() - 1);
+      for (int num : nums)
+      {
+         cout << num << " ";
+      }
+      cout << endl;
+   }
+   else if (query == "add")
+   {
+      vector<int> nums;
+      for (int i = 2; i < argc; i++)
+      {
+         nums.push_back(stoi(argv[i]));
+      }
+      int sum = magic::sum(nums);
+      cout << sum << endl;
+   }
+   else if (query == "mul")
+   {
+      vector<int> nums;
+      for (int i = 2; i < argc; i++)
+      {
+         nums.push_back(stoi(argv[i]));
+      }
+      int product = magic::mul(nums);
+      cout << product << endl;
+   }else{
+      string help = R"(
+      help:-
+      command-structre: bob <tags || keywords> <values> must be seperated by spaces if n
+      To test: greet
+      
+      -q : query (value must be included in quotes)
+      sort: sort n numbers [limitations: integers only]
+      roll: generates a random number with size of given value 
+      add: adds n values (just enter the values)
+      mul: multiplies n values (just enter the values)
+      )";
+      cout<< help<<endl;
+   }
+   return 0;
 }
