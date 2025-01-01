bb0468736a40b0fcbf0f1319c1874f8fffc72ced
Aarush-sharma <aarush.sharma.fpw@gmail.com>
2025-01-02 02:59:32 +0530

    added: http dependencies

--- src/tools.hpp
+++ src/tools.hpp
@@ -3,19 +3,25 @@
 #include "../modules/json.hpp"
 using namespace std;
 
-class magic {
-    public: static int roll(int size);
-
-    public: static void sort(std::vector<int> &nums,int low,int high);
-
-    public: static std::string greet();
-
-    public: static int sum(vector<int> &nums);
-
-    public: static int mul(vector<int> &nums);
-};
-
 std::string filter(const std::string &response);
 size_t getresponse(void *contents, size_t size, size_t nmemb, void *userp);
 std::string cook(std::string query);
 std::string defaultquery(std::string query);
+void write_file(const std::string& filename, const std::string& text);
+string read_file (std::string filename);
+vector<string> seperate_each_value(string s);
+
+class http {
+    public: static void send_get(string url, optional<string> headers);
+    public: static void send_post(string url, optional<string> headers, optional<string> body);
+    public: static void send_put(string url, optional<string> headers, optional<string> body);
+    public: static void send_delete(string url, optional<string> headers, optional<string> body);
+};
+
+class magic {
+    public: static int roll(int size);
+    public: static void sort(std::vector<int> &nums,int low,int high);
+    public: static std::string greet();
+    public: static int sum(vector<int> &nums);
+    public: static int mul(vector<int> &nums);
+};
